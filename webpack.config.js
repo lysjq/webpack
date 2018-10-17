e5130e50ff7a4e46a2f64e2f8118abd86cf8b7a9
lingys <39527733+lysjq@users.noreply.github.com>
2018-10-17 13:13:01 +0800

    完善webpack配置，可以server

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname+'/build'),
         filename: 'index.js',
-        publicPath:'/build' //代表了server自动监测时需要查看的文件，主html文件要和这相同，不然无法监测
+        publicPath:'/build/' //代表了server自动监测时需要查看的文件，主html文件要和这相同，不然无法监测// 图片资源路径 // dev-server路径 // 注意路径，没.
     },
     devtool:'eval-source-map',
     devServer: {
         contentBase: __dirname,
         compress:true,
         inline: true,
-        hot:true
+        // hot:true // 加上hot：true可能会报错
     },
     module: {
         rules: [
