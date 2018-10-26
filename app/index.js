3ff5b4ebda388b27e8beefd02a3a61f845a51b5f
lysjq <741779880@qq.com>
2018-10-26 15:06:58 +0800

    添加HtmlWebpackPlugin和CleanWebpackPlugin

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import { peoples } from './people';
 
 for (const people of peoples) {
-    let tbody = $('#table-people-tbody');
+    let tbody = document.getElementById('table-people-tbody');
+    let tr = document.createElement('tr');
+    tbody.append(tr);
     let ele = `<tr><td>${people.name}</td><td>${people.age}</td><td>${people.city}</td><td>${people.work}</td></tr>`;
-    tbody.append(ele);
-    if (people.age >= 25) {
-        ele.addClass('success');
-    }
+    tr.innerHTML = ele;
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-    entry: path.resolve(__dirname + '/app/index.js'),
+    entry: './app/index.js',
     output: {
-        path: path.resolve(__dirname+'/build'),
+        path: path.resolve(__dirname +'/build'),
         filename: 'index.js',
-        publicPath:'/build/' //代表了server自动监测时需要查看的文件，主html文件要和这相同，不然无法监测// 图片资源路径 // dev-server路径 // 注意路径，没.
+        // publicPath:'/build/' //代表了server自动监测时需要查看的文件，主html文件要和这相同，不然无法监测// 图片资源路径 // dev-server路径 // 注意路径，没.
     },
     devtool:'eval-source-map',
     devServer: {
-        contentBase: __dirname,
+        contentBase: path.resolve(__dirname + '/build'),
         compress:true,
         inline: true,
-        // hot:true // 加上hot：true可能会报错
+        hot:true // 需要插件HotModuleReplacementPlugin，否则加上hot：true可能会报错
     },
     module: {
         rules: [
@@ -31,6 +33,11 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.html'),
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin()
     ]
 };
